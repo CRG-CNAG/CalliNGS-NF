7d238ccaff0d8fdb7bc9c6c819a6c7eb181c6ec4
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2017-03-16 18:51:19 +0100

    Fixed validation test

--- validate-ci.sh
+++ validate-ci.sh
@@ -1,12 +1,13 @@
+#!/bin/bash 
 #
 # basic output files check 
 # improve it for a stronger validation check
 # 
-[[ -s results/ASER.out ]] && false
-[[ -s results/final.vcf ]] && false
-[[ -s results/gghist.out.pdf ]] && false
-[[ -s results/out.recode.vcf ]] && false
-[[ -s results/rep.final.vcf ]] && false
-[[ -s results/result.commonSNPs.diff.sites_in_files ]] && false
+[[ -s results/ASER.out ]] || false
+[[ -s results/final.vcf ]] || false
+[[ -s results/gghist.out.pdf ]] || false
+[[ -s results/out.recode.vcf ]] || false
+[[ -s results/rep.final.vcf ]] || false
+[[ -s results/result.commonSNPs.diff.sites_in_files ]] || false
 
 
